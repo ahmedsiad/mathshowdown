5877e663f0a27ed3179527c7d07fcd67c7bbf32d
ahmedsiad <ahmedsiadsemail@gmail.com>
2022-06-17 21:30:06 -0600

    Countdown

---
+++ client/src/components/Countdown.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import Typography from "@mui/material/Typography";
+
+const Countdown = (props) => {
+    const [timeLeft, setTimeLeft] = useState("");
+
+    useEffect(() => {
+        updateCountdown();
+
+        const intervalId = setInterval(() => {
+            updateCountdown();
+        }, 1000);
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const updateCountdown = () => {
+        const seconds = (props.timestamp - Date.now()) / 1000;
+        console.log(seconds);
+        if (seconds < 0) {
+            setTimeLeft("00:00:00");
+            return;
+        }
+
+        let timeStr = new Date(seconds * 1000).toISOString().substring(11, 19);
+        const hours = Math.floor(seconds / 3600);
+        if (hours >= 24) {
+            timeStr = hours.toString() + timeStr.substring(2);
+        }
+        setTimeLeft(timeStr);
+    }
+
+
+    return (
+        <div>
+            <Typography variant="h6" sx={{ color: "#555555" }}>{timeLeft}</Typography>
+        </div>
+    );
+}
+
+export default Countdown;
--- client/src/pages/Problem.jsx
+++ client/src/pages/Problem.jsx
@@ -7,6 +7,10 @@ import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import CircularProgress from "@mui/material/CircularProgress";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
+import Countdown from "../components/Countdown";
 
 
 const Problem = (props) => {
@@ -16,7 +20,11 @@ const Problem = (props) => {
     const [contest, setContest] = useState({});
     const [problem, setProblem] = useState({});
 
-    const k = "\\(4^2\\)";
+    const [inputs, setInputs] = useState({ answer: "" });
+    const [errors, setErrors] = useState({ answer: false });
+    const [helpers, setHelpers] = useState({ answer: "" });
+
+    const k = "\\(4^{127} + 5^{129} + 7^{231}\\)";
 
     useEffect(() => {
         const { contest_id, problem_index } = params;
@@ -44,6 +52,16 @@ const Problem = (props) => {
         }
     }, [loading]);
 
+
+    const handleChange = (event) => {
+        setInputs({ ...inputs, [event.target.name]: event.target.value });
+        setErrors({ ...errors, [event.target.name]: false });
+        setHelpers({ ...helpers, [event.target.name]: "" });
+    }
+
+    const submit = (event) => {
+
+    }
     return (
         <div>
             {loading &&
@@ -60,9 +78,41 @@ const Problem = (props) => {
                                 <Grid item xs={12} sx={{ textAlign: "center" }}>
                                     <Typography variant="h5"><b>Problem {problem.problem_index}</b>: {problem.title}</Typography>
                                 </Grid>
+                                <Divider sx={{ width: "100%", marginTop: "10px" }} />
+                                <Grid item xs={12} sx={{ marginTop: "10px" }}>
+                                    <Typography>What is the integer formed by the rightmost two digits of the integer equal to {k}?</Typography>
+                                </Grid>
+                                <Grid item xs={12} sx={{ marginTop: "30px", textAlign: "center" }}>
+                                    <TextField
+                                        value={inputs.answer}
+                                        name="answer"
+                                        onChange={handleChange}
+                                        label="Answer"
+                                        type="number"
+                                        variant="standard"
+                                        error={errors.answer}
+                                        helperText={helpers.answer}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sx={{ marginTop: "10px", textAlign: "center" }}>
+                                    <Button variant="contained" onClick={submit}>Lock In</Button>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Paper square sx={{ padding: "12px" }} elevation={3}>
+                            <Grid container spacing={1} sx={{ width: "100%", margin: 0, textAlign: "center" }}>
+                                <Grid item xs={12}>
+                                    <Link
+                                        href={`/contest/${contest.id}`}
+                                        variant="h6"
+                                        sx={{ textDecoration: "none" }}>{contest.title} (Div. {contest.division})</Link>
+                                </Grid>
                                 <Divider sx={{ width: "100%" }} />
                                 <Grid item xs={12}>
-                                    <Typography>What is the integer formed by the rightmost two digits of the integer equal to \(4^2\) and {k}</Typography>
+                                    <Typography variant="body1" sx={{ fontWeight: "lighter" }}>Time Remaining:</Typography>
+                                    <Countdown timestamp={1655529705753} />
                                 </Grid>
                             </Grid>
                         </Paper>
