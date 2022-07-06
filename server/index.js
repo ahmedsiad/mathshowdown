be91c67e4fd5ce4cc79a3cb55a8a4d8a9658f2e3
ahmedsiad <ahmedsiadsemail@gmail.com>
2022-07-05 20:56:57 -0600

    Heroku setup

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
+    app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 // ROUTES
